4c4cc186e9907343c5a1f21e4e4e2e2a5e51fc72
seyoungyook <xqd1219@naver.com>
2019-09-24 00:55:39 +0900

    firstproject

---
+++ reallyreallyend.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int x;
+	char y;
+	short z;
+	float a;
+	double b;
+	
+	printf("bytes of int : %d\n", sizeof(x));
+	printf("bytes of char : %d\n", sizeof(y));
+	printf("bytes of short : %d\n", sizeof(z));
+	printf("bytes of float : %d\n", sizeof(a));
+	printf("bytes of double : %d\n", sizeof(b));
+	
+	return 0;
+}
